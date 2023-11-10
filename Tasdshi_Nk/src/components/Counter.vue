5f654c5624781b39d813e3c930cd7576974be7b6
Nonthawt20 <Nonthawt20@gmail.com>
2023-11-10 23:41:23 +0700

    update Form
    - watch , computed การจัดการ Muti components
    - add validate name and email

---
+++ Tasdshi_Nk/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+import { ref  ,defineProps} from 'vue';
+
+const count = ref(0)
+
+
+const increaseCounter = () =>{
+    count.value = count.value + 1
+}
+
+
+defineProps({
+    messageText : String,
+    changeMassage : Function
+})
+</script>
+<template>
+
+    <div>Hello {{ messageText }}</div>
+    <div> 
+        <input type="text" v-model="message">
+        <button @click="changeMassage(message)">changeMassage from counter</button>
+    </div>
+<button @click="increaseCounter">Click {{ count }}</button>
+</template>
